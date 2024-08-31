2a342e893c303ee62a827aa71b0bd7054775fa25
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-08-31 13:05:49 +0530

    navbar statistics section

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/specific/landingPage/Navbar/Navbar";
 import Welcome from "../../components/specific/landingPage/welcome/Welcome";
+import Statistics from "../../components/specific/landingPage/statistics/Statistics";
 
 export default function LandingPage() {
   return (
     <div>
       <Navbar />
       <Welcome />
+      <Statistics />
     </div>
   );
 }
