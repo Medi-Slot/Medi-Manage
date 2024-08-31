41fcb5d81b64b92ea0e0592c2c8e214d449a837b
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-08-31 10:37:18 +0530

    nav initial commit landingpage

--- src/components/specific/landingPage/navbar/Navbar.jsx
+++ src/components/specific/landingPage/navbar/Navbar.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import MediManage from "../../../../assets/images/Medi-Manage_logo.png";
+import "./style.css";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="landing-page-navbar">
+      <div className="landing-page-navbar-logo">
+        <img src={MediManage} alt="Medi-Manage logo" />
+        <h2>Medi-Manage</h2>
+      </div>
+    </div>
+  );
 }
