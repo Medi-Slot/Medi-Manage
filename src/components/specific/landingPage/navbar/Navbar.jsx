f75aa569a9e2966f53dd118cfe4ac01e2848a26b
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-08-31 12:09:18 +0530

    adding first section and navbar in landing

--- src/components/specific/landingPage/navbar/Navbar.jsx
+++ src/components/specific/landingPage/navbar/Navbar.jsx
@@ -9,6 +9,23 @@ export default function Navbar() {
         <img src={MediManage} alt="Medi-Manage logo" />
         <h2>Medi-Manage</h2>
       </div>
+      <div className="landing-page-navbar-list">
+        <h3>About</h3>
+        <h3>Features</h3>
+        <h3>Testimonials</h3>
+      </div>
+      <div className="landing-page-navbar-login-box">
+        <button className="landing-page-navbar-login-box-button-1">
+          <span className="landing-page-navbar-login-box-button-1-txt">
+            Sign In
+          </span>
+        </button>
+        <button className="landing-page-navbar-login-box-button-2">
+          <span className="landing-page-navbar-login-box-button-2-txt">
+            Sign Up
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../../components/specific/landingPage/Navbar/Navbar";
+import Welcome from "../../components/specific/landingPage/welcome/Welcome";
 
 export default function LandingPage() {
   return (
     <div>
       <Navbar />
-      LandingPage
+      <Welcome />
     </div>
   );
 }
