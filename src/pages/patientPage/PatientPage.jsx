bf3198e7de2bc019da54c89dbfc2a141c44f3d94
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-09-01 18:07:28 +0530

    commit

--- src/pages/patientPage/PatientPage.jsx
+++ src/pages/patientPage/PatientPage.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import PatientData from "../../components/specific/patientPage/patientData/PatientData";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setTitle } from "../../redux/slices/titleSlice";
 export default function PatientPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setTitle("Patients")); //use the title u need
+  }, [dispatch]);
+
   return (
     <div>
       <PatientData />
