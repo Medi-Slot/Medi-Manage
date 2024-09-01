62c9d6ac4d0cbf89d5bd7e57ccc4d1d54564d4b8
Surya-0804 <surya.abothula@gmail.com>
2024-09-01 21:52:46 +0530

    created setslot and beds selection basic functionality

---
+++ src/components/common/upcomingAppointment/UpcomingAppointment.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const UpcomingAppointment = ({size}) => {
+  return (
+    <div className="doctor-profile-upcoming-appointments">
+        <h4 style={{fontSize:size}}>Upcoming Appointment</h4>
+        <div className="doctor-profile-date-line">
+          <span className="doctor-profile-appointment-date">July 30, 2022</span>
+        </div>
+        <ul>
+          <li>
+            <div className="doctor-profile-appointment-info">
+              <span className="doctor-profile-time">08:30 am - 09:00 am</span>
+              <div className="doctor-profile-appointment-details">
+                <p className="doctor-profile-patient-name">Harikesh A</p>
+                <p className="doctor-profile-doctor-name">Dr. Shiva</p>
+              </div>
+            </div>
+          </li>
+          <hr className="doctor-profile-appointment-divider" />
+          <li>
+            <div className="doctor-profile-appointment-info">
+              <span className="doctor-profile-time">09:00 am - 09:30 am</span>
+              <div className="doctor-profile-appointment-details">
+                <p className="doctor-profile--name">Surya A</p>
+                <p className="doctor-profile-doctor-name">Dr. Venkat</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+  )
+}
+
+export default UpcomingAppointment
--- src/pages/appointmentsPage/Appointments.jsx
+++ src/pages/appointmentsPage/Appointments.jsx
@@ -1,18 +1,23 @@
-import React, { useEffect } from 'react'
-import Header from '../../components/common/header/Header'
-import { useDispatch } from 'react-redux';
-import { setTitle } from '../../redux/slices/titleSlice';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setTitle } from "../../redux/slices/titleSlice";
+import UpcomingAppointment from "../../components/common/upcomingAppointment/UpcomingAppointment";
+import "./Appointments.css";
+import PatientData from "../../components/specific/appointments/patientData/PatientData";
 
 const Appointments = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(setTitle("Appointments"));
   }, [dispatch]);
-  return (
-    <div>
-    </div>
-  )
-}
 
-export default Appointments
+  return (
+    <div className="appointment-page-container">
+      <PatientData/>
+      <UpcomingAppointment size="2rem"/>
+    </div>
+  );
+};
+
+export default Appointments;
