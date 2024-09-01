929fa894dbd4cc492bd99ea551e47d1ad23f6332
Mahesh-addagatla <maheshaddagatla1234@gmail.com>
2024-09-01 08:59:11 +0530

    dashboard last component PatientData

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Navbar from '../components/common/navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import DoctorProfile from '../components/specific/doctorprofile/DoctorProfile';
 
 const Layout = () => {
   return (
     <>
       <div className="layoutmain" style={{ display: 'flex', flexDirection: 'row' }}>
         <Navbar />
-        <div className='layout-container' style={{ marginLeft: '1%', width: '100%' }}>
+        <div className='layout-container' style={{ width: '67%' }}>
           <Outlet /> {/* This renders the nested routes (Inventory, Dashboard, etc.) */}
         </div>
+        <DoctorProfile />
       </div>
     </>
   );
