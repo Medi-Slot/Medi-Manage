c33a4e15ad7677e4e1a697f1ae0ad87971136f15
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-09-01 18:25:11 +0530

    layout for dahsboard

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
+import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "../components/common/navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import Header from "../components/common/header/Header";
 import DoctorProfile from "../components/specific/doctorprofile/DoctorProfile";
 import NewPatient from "../components/specific/patientPage/newPatient/NewPatient";
-import Header from "../components/common/header/Header";
 
 const Layout = () => {
-  const [newPatient, setnewPatient] = useState(false);
+  const [newPatient, setNewPatient] = useState(false);
+  const location = useLocation();
 
   const handleIconClick = () => {
-    setnewPatient(true);
+    setNewPatient(true);
   };
+
   return (
     <>
       <div
@@ -19,20 +21,23 @@ const Layout = () => {
       >
         <Navbar />
         <div className="layout-container" style={{ width: "67%" }}>
-          <Header/>
-          <Outlet context={{ handleIconClick }} />{" "}
-          {/* This renders the nested routes (Inventory, Dashboard, etc.) */}
+          <Header />
+          <Outlet context={{ handleIconClick }} />
         </div>
-        <section
-          style={{
-            width: "300px",
-            backgroundColor: "white",
-            boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
-            padding: "20px",
-          }}
-        >
-          {newPatient && <NewPatient />}
-        </section>
+        {location.pathname === "/dashboard" ? (
+          <DoctorProfile />
+        ) : (
+          <section
+            style={{
+              width: "300px",
+              backgroundColor: "white",
+              boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+              padding: "20px",
+            }}
+          >
+            {newPatient && <NewPatient />}
+          </section>
+        )}
       </div>
     </>
   );
