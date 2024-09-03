2057d8cecfcc42e43241c109cfaf3a7cc0bb60d7
Mahesh-addagatla <maheshaddagatla1234@gmail.com>
2024-09-03 11:36:20 +0530

    Inventory fetching

--- src/components/specific/patientPage/patientData/PatientData.jsx
+++ src/components/specific/patientPage/patientData/PatientData.jsx
@@ -114,10 +114,10 @@ function PatientData() {
                     height: "50vh",
                   }}
                 >
-                  <div class="three-body">
-                    <div class="three-body__dot"></div>
-                    <div class="three-body__dot"></div>
-                    <div class="three-body__dot"></div>
+                  <div className="three-body">
+                    <div className="three-body__dot"></div>
+                    <div className="three-body__dot"></div>
+                    <div className="three-body__dot"></div>
                   </div>
                 </div>
               </td>{" "}
