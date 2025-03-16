0720901f4f7591b27b99c83da83e8733bcf869c1
Surya-0804 <surya.abothula@gmail.com>
2025-03-16 20:18:49 +0530

    Refactor import statements in LandingPage component for consistency

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Navbar from "../../components/specific/landingPage/Navbar/Navbar";
-import Welcome from "../../components/specific/landingPage/welcome/Welcome";
-import Statistics from "../../components/specific/landingPage/statistics/Statistics";
-import ChooseUs from "../../components/specific/landingPage/chooseus/ChooseUs";
-import Facilities from "../../components/specific/landingPage/facilities/Facilities";
-import Trust from "../../components/specific/landingPage/trust/Trust";
-import Footer from "../../components/specific/landingPage/footer/Footer";
+import React from 'react';
+import Navbar from '../../components/specific/landingPage/navbar/Navbar';
+import Welcome from '../../components/specific/landingPage/welcome/Welcome';
+import Statistics from '../../components/specific/landingPage/statistics/Statistics';
+import ChooseUs from '../../components/specific/landingPage/chooseus/ChooseUs';
+import Facilities from '../../components/specific/landingPage/facilities/Facilities';
+import Trust from '../../components/specific/landingPage/trust/Trust';
+import Footer from '../../components/specific/landingPage/footer/Footer';
 
 export default function LandingPage() {
   return (
